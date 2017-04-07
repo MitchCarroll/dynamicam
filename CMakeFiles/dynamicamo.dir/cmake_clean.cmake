d63892d68eff4449b6b75df4e75e4275649fbd37
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2017-04-07 15:44:04 -0500

    moved to cmake

---
+++ CMakeFiles/dynamicamo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dynamicamo.dir/src/dynamicamo.cpp.o"
+  "dynamicamo.pdb"
+  "dynamicamo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dynamicamo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
