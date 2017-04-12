cfd5dcdf54e5944d1c4c8d611f739aeb31fb6247
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2017-04-12 12:58:03 -0500

    removed a bunch of C++ stuff.

---
+++ src/dynamicamo.rs
@@ -0,0 +1,32 @@
+/******************************************************************\
+*   Copyright: C. Mitch Carroll                                    *
+*      All rights reserved.                                        *
+\******************************************************************/
+
+/* 
+dynamically creates a digital camoflage pattern in XPM format
+based on a text input file containing the necessary parameters:
+  Number of layers
+  Dimensions of each layer
+  Number of colors
+  Random seed
+  RGB values for each color
+  Format as follows:
+
+  SIZE_X SIZE_Y
+  LAYERS COLORS
+  SEED
+  L1X L1Y
+  L2X L2Y
+  L3X L4Y
+  ... ...
+  RRGGBB
+  RRGGBB
+  RRGGBB
+  ......
+
+*/
+
+let size_x: i16;
+
+size_x = 42;
