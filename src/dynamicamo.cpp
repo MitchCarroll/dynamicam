f260f028058d120b2952ff31ad8c868ff6cec781
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2017-04-12 11:07:20 -0500

    made some changes to cmake and the source
    helpful comment, eh? :)

--- src/dynamicamo.cpp
+++ src/dynamicamo.cpp
@@ -41,9 +41,9 @@ const string pal[]={
   "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","`","~",
   ",","<",".",">","/","?",";",":","'","\"","!","@","#","$","%","^",
   "&","*","(",")","-","_","=","+","[","{","]","}","\\","|"};
-//FIXME: load palette from file. Maybe not? This way, it should be baked into
+//FIXME: load palette from file. Maybe not? This way, it should be baked long into
 //       the binary, so it's probably just about optimal, unless we are going
-//       for a small memory footprint, though this is pretty dang small to begin
+//       for a small memory footprlong int, though this is pretty dang small to begin
 //       with...
 
 int main(int argc, char **argv)
@@ -65,15 +65,16 @@ int main(int argc, char **argv)
   }
   ifstream infile; //the input file stream
   ofstream outfile;//the file our program will generate;
-  int size_x, size_y, num_colors, num_layers; //the number of colors and 
+  long int size_x, size_y, num_colors, num_layers; //the number of colors and 
                                               //layers to create, and the 
                                               //final image size
-  string *colors; //an array of colors
-  int **dimensions; //an array of the dimensions of each layer
-  int ***layers; //the container for all the layers
-  int **image; //final image array
-
   int seed;
+  
+  string *colors; //an array of colors
+  long int **dimensions; //an array of the dimensions of each layer
+  long int ***layers; //the container for all the layers
+  long int **image; //final image array
+
 
   if(argc >= 2) 
     infile.open(argv[1]); //FIXME: use command line parsing, e.g. -in=
@@ -88,51 +89,50 @@ int main(int argc, char **argv)
   infile >> num_layers; //read number of layers
   infile >> num_colors; //read number of colors
   infile >> seed; //read the random seed
-  layers = new int**[num_layers]; //initialize the first dimension
-  dimensions = new int*[num_layers];
-  for(int l=0;l<num_layers;l++)
-    dimensions[l]=new int[2]; //dimensions array allocated
-  for (int layer=0;layer<num_layers;layer++) { //for each layer
+  layers = new long int**[num_layers]; //initialize the first dimension
+  dimensions = new long int*[num_layers];
+  for(long int l=0;l<num_layers;l++)
+    dimensions[l]=new long int[2]; //dimensions array allocated
+  for (long int layer=0;layer<num_layers;layer++) { //for each layer
     infile >> dimensions[layer][0]; //read the dimensions of the layer from
     infile >> dimensions[layer][1]; //...the file
-    layers[layer] = new int*[dimensions[layer][0]]; //dimension 2
-    for(int x=0;x<dimensions[layer][0];x++) //dimension 3
-      layers[layer][x] = new int[dimensions[layer][1]];
+    layers[layer] = new long int*[dimensions[layer][0]]; //dimension 2
+    for(long int x=0;x<dimensions[layer][0];x++) //dimension 3
+      layers[layer][x] = new long int[dimensions[layer][1]];
   } //layer arrays initialized
   colors = new string[num_colors]; //initialize color pallette array
-  for(int c=0;c<num_colors;c++) //for each color
+  for(long int c=0;c<num_colors;c++) //for each color
     infile >> colors[c]; //read color string from file
   infile.close(); //we really shouldn't need it anymore
 
   //sort colors by value
   
   srandom(seed);  
-
-  for(int l=0;l<num_layers;l++) 
-    for(int x=0;x<dimensions[l][0];x++)
-      for(int y=0;y<dimensions[l][1];y++) 
-	layers[l][x][y]=random()%num_colors;
-  image=new int*[size_x];
-  for(int x=0;x<size_x;x++)
-    image[x]=new int[size_y];//the array for the final image data
-  for(int x=0;x<size_x;x++)
-    for(int y=0;y<size_y;y++)
+  for(long int l=0;l<num_layers;l++) 
+    for(long int x=0;x<dimensions[l][0];x++)
+      for(long int y=0;y<dimensions[l][1];y++) 
+	layers[l][x][y]=random()%(num_colors+1);
+  image=new long int*[size_x];
+  for(long int x=0;x<size_x;x++)
+    image[x]=new long int[size_y];//the array for the final image data
+  for(long int x=0;x<size_x;x++)
+    for(long int y=0;y<size_y;y++)
       image[x][y]=0; //initialize all elements of the image to 0
   //Scale each layer to the final size, and add the layers
-  for(int l=0;l<num_layers;l++) 
-    for(int x=0;x<size_x;x++) 
-      for(int y=0;y<size_y;y++) 
+  for(long int l=0;l<num_layers;l++) 
+    for(long int x=0;x<size_x;x++) 
+      for(long int y=0;y<size_y;y++) 
 	image[x][y]+=layers[l]
-	  [x/(size_x/dimensions[l][0])]
-	  [y/(size_y/dimensions[l][1])]; 
+                       [x/(size_x/dimensions[l][0])]
+                       [y/(size_y/dimensions[l][1])]; 
   //Average the values of the final array
-  for(int x=0;x<size_x;x++) 
-    for(int y=0;y<size_y;y++) 
+  for(long int x=0;x<size_x;x++) 
+    for(long int y=0;y<size_y;y++) 
       image[x][y]=image[x][y]/num_layers;
   //The pattern has now been generated. All that remains is to write it
   // to a file, and delete our structures.
   cout << argc << endl;
-  for(int a=0;a<argc;a++)
+  for(long int a=0;a<argc;a++)
     cout << argv[a] << endl;
   cout << "opening outfile\n";
   if(argc >= 3)
@@ -148,33 +148,32 @@ int main(int argc, char **argv)
 	  << "static char * camo_xpm[] = {" << endl
 	  << "\"" << size_x << " " << size_y << " " 
 	  << num_colors << " 1\"," << endl;
-  for(int c=0;c<num_colors;c++)
+  for(long int c=0;c<num_colors;c++)
     outfile << "\"" << pal[c] << " 	c #" << colors[c] << "\"," << endl;
 
-  for(int x=0;x<size_x;x++) {
+  for(long int x=0;x<size_x;x++) {
     outfile << "\"";
-    for(int y=0;y<size_y;y++)
+    for(long int y=0;y<size_y;y++)
       outfile << pal[image[x][y]];
     outfile << "\"," << endl;
   }
   outfile << "};" << endl;
   outfile.close();
   //  delete layers,colors,dimensions, and image
-  // !!!!!TODO!!!! Check for memory leaks!
-  for(int l=0;l<num_layers;l++) {
-    for(int x=0;x<dimensions[l][0];x++) 
+  for(long int l=0;l<num_layers;l++) {
+    for(long int x=0;x<dimensions[l][0];x++) 
       delete [] layers[l][x];
     delete [] layers[l];
   }
   delete [] layers;
-  for(int l=0;l<num_layers;l++) 
+  for(long int l=0;l<num_layers;l++) 
     delete [] dimensions[l];
 
   delete [] dimensions;
 
   delete [] colors;
 
-  for(int x=0;x<size_x;x++) 
+  for(long int x=0;x<size_x;x++) 
     delete [] image[x];
 
   delete [] image;
