dbb9267c451fdbcd307849c8bcf6401d1aaaeac2
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2017-04-13 11:42:41 -0500

    changed batch.sh
    now points to the debug binary

--- batch.sh
+++ batch.sh
@@ -1,4 +1,4 @@
 for f in input/*.cam ; 
     do FILENAME=`basename ${f%%.*}`; 
-    ./dynamicamo input/${FILENAME}.cam output/${FILENAME}.xpm; 
+    ./target/debug/dynamicam input/${FILENAME}.cam output/${FILENAME}.xpm; 
 done
