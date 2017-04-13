45c7a4f83305e62f872bea6d1f2b921bb494cb15
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2017-04-13 11:36:13 -0500

    made some changes to the header parsing code.
    it compiles now, and reads the .cam header data correctly.

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,60 @@ based on a text input file containing the necessary parameters:
   ......
 
 */
+extern crate rand;
+use std::env;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+//use std::io::prelude::*;
+//use std::path::Path;
+//use std::error::Error;
+//use rand::{Rng, SeedableRng, StdRng};
+
 fn main()
 {
-    let size_x: i16;
+    let mut args: env::Args = env::args();
+    check_args(&args);
 
-    size_x = 42;
+    let mut infile = match File::open(args.nth(1).unwrap_or_default()) {
+        Ok(file) => file,
+        Err(why) => panic!(why),
+    };
+    let mut in_data = String::new();
+
+    infile.read_to_string(&mut in_data);
+    let mut split = in_data.split_whitespace();
+    let size_x: i16 = split.next().unwrap().to_string().parse().unwrap();
+    let size_y: i16 = split.next().unwrap().to_string().parse().unwrap();
+    let num_layers: i8 = split.next().unwrap().to_string().parse().unwrap();
+    let num_colors: i8 = split.next().unwrap().to_string().parse().unwrap();
+    let seed: i64 = split.next().unwrap().to_string().parse().unwrap();
+
+    
+    println!("{},{}",size_x,size_y);
+    println!("{},{}",num_layers,num_colors);
+    println!("{}",seed);
+
+}
+
+fn check_args(args: &env::Args)
+{
+    if args.len() < 3 {
+        println!("please provide an input file in the following format:");
+        println!("SIZE_X SIZE_Y");
+        println!("LAYERS COLORS" );
+        println!("SEED");
+        println!("L1X L1Y" );
+        println!("L2X L2Y" );
+        println!("L3X L4Y" );
+        println!("... ..." );
+        println!("RRGGBB");
+        println!("RRGGBB" );
+        println!("RRGGBB" );
+        println!("......" );
+        println!();
+        println!("output is in XPM format" );
+        println!("USAGE: dynamicamo <infile> <outfile>");
+        std::process::exit(1);
+    }
 }
